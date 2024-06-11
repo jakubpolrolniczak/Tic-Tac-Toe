5791f39f043652ef72d9c04f5ea73c0d7e4eb6d9
jakubpolrolniczak <kuba.rolnik@gmail.com>
2024-06-11 19:01:07 +0200

    Game logic stage 1

--- game/main.js
+++ game/main.js
@@ -1,6 +1,6 @@
 console.log('Test JS')
 
-//button reset
+//reset button
 const btnOption = document.querySelector('.resetBtn');
 
 btnOption.addEventListener('click', () => {
@@ -31,47 +31,111 @@ function gameButton(value) {
 
     switch (value) {
         case 0:
-            console.log()
-            document.getElementById("filed1").innerText = "X"
-            console.log("Input ")
-            break
+            console.log(document.getElementById("filed1").innerHTML)
+            if (document.getElementById("filed1").innerHTML !== '') {
+                console.log('Filed occupied')
+                break
+            } else {
+                console.log('Input Value')
+                document.getElementById("filed1").innerText = "X"
+                break
+            }
         case 1:
-            document.getElementById("filed2").innerText = "X"
-            break
+            console.log(document.getElementById("filed2").innerHTML)
+            if (document.getElementById("filed2").innerHTML !== '') {
+                console.log('Filed occupied')
+                break
+            } else {
+                console.log('Input Value')
+                document.getElementById("filed2").innerText = "X"
+                break
+            }
         case 2:
-            document.getElementById("filed3").innerText = "X"
-            break
+            console.log(document.getElementById("filed3").innerHTML)
+            if (document.getElementById("filed3").innerHTML !== '') {
+                console.log('Filed occupied')
+                break
+            } else {
+                console.log('Input Value')
+                document.getElementById("filed3").innerText = "X"
+                break
+            }
         case 3:
-            document.getElementById("filed4").innerText = "X"
-            break
+            console.log(document.getElementById("filed4").innerHTML)
+            if (document.getElementById("filed4").innerHTML !== '') {
+                console.log('Filed occupied')
+                break
+            } else {
+                console.log('Input Value')
+                document.getElementById("filed4").innerText = "X"
+                break
+            }
         case 4:
-            document.getElementById("filed5").innerText = "X"
-            break
+            console.log(document.getElementById("filed5").innerHTML)
+            if (document.getElementById("filed5").innerHTML !== '') {
+                console.log('Filed occupied')
+                break
+            } else {
+                console.log('Input Value')
+                document.getElementById("filed5").innerText = "X"
+                break
+            }
         case 5:
-            document.getElementById("filed6").innerText = "X"
-            break
+            console.log(document.getElementById("filed6").innerHTML)
+            if (document.getElementById("filed6").innerHTML !== '') {
+                console.log('Filed occupied')
+                break
+            } else {
+                console.log('Input Value')
+                document.getElementById("filed6").innerText = "X"
+                break
+            }
         case 6:
-            document.getElementById("filed7").innerText = "X"
-            break
+            console.log(document.getElementById("filed7").innerHTML)
+            if (document.getElementById("filed7").innerHTML !== '') {
+                console.log('Filed occupied')
+                break
+            } else {
+                console.log('Input Value')
+                document.getElementById("filed7").innerText = "X"
+                break
+            }
         case 7:
-            document.getElementById("filed8").innerText = "X"
-            break
+            console.log(document.getElementById("filed8").innerHTML)
+            if (document.getElementById("filed8").innerHTML !== '') {
+                console.log('Filed occupied')
+                break
+            } else {
+                console.log('Input Value')
+                document.getElementById("filed8").innerText = "X"
+                break
+            }
         case 8:
-            document.getElementById("filed9").innerText = "X"
-            break
-
+            console.log(document.getElementById("filed9").innerHTML)
+            if (document.getElementById("filed9").innerHTML !== '') {
+                console.log('Filed occupied')
+                break
+            } else {
+                console.log('Input Value')
+                document.getElementById("filed9").innerText = "X"
+                break
+            }
     }
-
 }
 
 
 
+//input values = if user clicked on field then input X or O alternately | always start X
+
+//checking last move
+const checkValues = btnxo.forEach((element) => console.log(element.innerHTML));
+console.log(checkValues)
 
 
 
-//check if field is busy
 
 
 
 
-//input values = if user clicked on field then  input in this filed X or O
+//check winner
+
